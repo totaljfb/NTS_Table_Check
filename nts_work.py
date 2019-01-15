74923b4c10b95ed540bd39b854a8e7ffb059bcec
Jason Zhang <totaljfb@gmail.com>
2019-01-15 17:11:59 -0500

    minor changes2

--- nts_work.py
+++ nts_work.py
@@ -20,37 +20,57 @@ from openpyxl import load_workbook
 from collections import Counter
 #change the file name to work on target file
 wb = load_workbook("C:\\Users\\Jason.Zhang\\Dropbox\\NTS-\\2019\\Q1\\First Review\\"
-                    +"table_01_48PG2-12&2-13&3-5.xlsx")
+                    +"table_01_52.xlsx")
+
 sheet_list = wb.sheetnames
+
 #sheet_list[0]: the first sheet you want to check
 #sheet_list[1]: the second sheet you want to check, etc
 sheet1 = wb.get_sheet_by_name(sheet_list[1])
+
+#sometimes the max_row returns a large number, temperary solution is
+#to manually set the row count here, can be improved in the future
 max_row1 = sheet1.max_row
 max_col1 = sheet1.max_column
+#example for manually setting the row count
+#max_row1 = 45
 
 for i in range(1, max_row1):
     bold_list = []
     for j in range(1, max_col1):
-        cell = sheet1.cell(row = i, column =j)
-        bold_list.append(str(cell.font.bold))
-    print("Row " + str(i) + ", height: " + str(sheet1.row_dimensions[i].height)
-    + " font bold information : " + Counter(bold_list).__str__())
+        cell = sheet1.cell(row = i, column = j)
+        #if the cell is not empty, then get the property and append to list
+        if cell.value:
+            bold_list.append(str(cell.font.bold))
+    #if the list is not empty, then print it
+    if bold_list:
+        print("Row " + str(i) + ", height: "
+        + str(sheet1.row_dimensions[i].height)
+        + " font bold information : " + Counter(bold_list).__str__())
 print('\n')
 for i in range(1, max_row1):
     font_name_list = []
     for j in range(1, max_col1):
         cell = sheet1.cell(row = i, column =j)
-        font_name_list.append(cell.font.name)
-    print("Row " + str(i) + ", font name information: "
-    + Counter(font_name_list).__str__())
+        #if the cell is not empty, then get the property and append to list
+        if cell.value:
+            font_name_list.append(cell.font.name)
+    #if the list is not empty, then print it
+    if font_name_list:
+        print("Row " + str(i) + ", font name information: "
+        + Counter(font_name_list).__str__())
 print('\n')
 for i in range(1, max_row1):
     font_size_list = []
     for j in range(1, max_col1):
         cell = sheet1.cell(row = i, column =j)
-        font_size_list.append(int(cell.font.size))
-    print("Row " + str(i) + ", font size information: "
-    + Counter(font_size_list).__str__())
+        #if the cell is not empty, then get the property and append to list
+        if cell.value:
+            font_size_list.append(int(cell.font.size))
+    #if the list is not empty, then print it
+    if font_size_list:
+        print("Row " + str(i) + ", font size information: "
+        + Counter(font_size_list).__str__())
 
 
 
